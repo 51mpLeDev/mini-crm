42d7908e8c337c798ea468f6b2631dd27372aef6
51mpleDev <firdavs02shukurov@gmail.com>
2026-02-07 13:06:28 +0500

    add customers and tickets models and migrations

---
+++ app/Models/Ticket.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'subject',
+        'message',
+        'status',
+        'answered_at',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
